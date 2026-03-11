36194edf04ed1a79ce271331a4e3c92105a9ddaa
Задорин Артем <artfan95@gmail.com>
2026-03-11 22:46:59 +0700

    feat: Добавлен линтер

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
+import stylistic from '@stylistic/eslint-plugin'
+
+export default defineConfig([
+  stylistic.configs.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.node },
+  },
+])
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
-import readlineSync from 'readline-sync';
+import readlineSync from 'readline-sync'
 
-export function sayHello(){
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}`);
+export function sayHello() {
+  const name = readlineSync.question('May I have your name? ')
+  console.log(`Hello, ${name}`)
 }
